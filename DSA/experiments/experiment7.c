4d2e64ccbd51f6c46933cb05c271a5340e950c83
Poornima-Prerna <prernajangid1@gmail.com>
2024-11-27 20:56:32 +0530

    Add files via upload

---
+++ DSA/experiments/experiment7.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void preorder(struct Node* root) {
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void postorder(struct Node* root) {
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+struct Node* getLeftChild(struct Node* root) {
+    return root->left;
+}
+
+struct Node* getRightChild(struct Node* root) {
+    return root->right;
+}
+
+int main() {
+    struct Node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("Preorder traversal: ");
+    preorder(root);
+    printf("\n");
+
+    printf("Inorder traversal: ");
+    inorder(root);
+    printf("\n");
+
+    printf("Postorder traversal: ");
+    postorder(root);
+    printf("\n");
+
+    struct Node *leftChild = getLeftChild(root);
+    struct Node *rightChild = getRightChild(root);
+
+    printf("Left child of root: %d\n", leftChild->data);
+    printf("Right child of root: %d\n", rightChild->data);
+
+    return 0;
+}
